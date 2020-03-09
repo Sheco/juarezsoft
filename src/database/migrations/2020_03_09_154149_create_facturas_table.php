fcf629e2488b45afdc1ccc1b561dfff123d9bda7
Sergio Duran <sergio@tiene.info>
2020-03-09 15:00:52 -0600

    Estructura de la tabla de facturacion

---
+++ src/database/migrations/2020_03_09_154149_create_facturas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacturasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('venta_id');
+            $table->enum('status', ['normal', 'cancelada']);
+            $table->string('rfc');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->integer('codigopostal');
+            $table->string('telefono');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facturas');
+    }
+}
