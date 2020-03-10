42b16af67353b39993a9e948e5795583d5c9b3da
Sergio Duran <sergio@tiene.info>
2020-03-10 06:16:09 -0600

    Link al punto de venta en la barra superior

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -50,6 +50,11 @@
                                 </li>
                             @endif
                         @else
+                        @can('vender')
+                        <li class="nav-itme">
+                            <a class="nav-link" href="{{ route("puntodeventa.index") }}">Punto de venta</a>
+                        </li>
+                        @endcan
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
