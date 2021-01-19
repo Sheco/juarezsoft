376d0dd1150c0b5679bb750e3eedba546334ad98
Sergio Durán <sergio@tiene.info>
2021-01-19 13:27:27 -0600

    Hacer una inicalizacion completa en el init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
-# First, copy src/.env.example to src/.env and then edit it
-# Then docker-compose up -d
-# Then run this script
+BASEDIR=$( realpath $(dirname $0 ))
+cd $BASEDIR
 
+[ ! -e src/.env ] && cp src/.env.example src/.env
+
+docker-compose up -d
 docker-compose exec app php artisan key:generate
 docker-compose exec -u www-data app touch /var/www/storage/database.sqlite
 docker-compose exec -u www-data app php artisan migrate:fresh
